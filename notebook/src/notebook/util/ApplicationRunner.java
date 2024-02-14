21fb803b747f48c3174b8f695a2c532f57d52e9e
Isaev_Anton <gsgmad@mail.ru>
2024-02-14 19:13:20 +0400

    change some file

--- notebook/src/notebook/util/ApplicationRunner.java
+++ notebook/src/notebook/util/ApplicationRunner.java
@@ -11,13 +11,10 @@ import java.util.List;
 import static notebook.util.DBConnector.createDB;
 
 public class ApplicationRunner {
-    public static final List<String> HASH = new ArrayList<>();
-
     private ApplicationRunner() {
         createDB();
         GBRepository repository = new UserRepository();
         UserController controller = new UserController(repository);
-        HASH.addAll(controller.readAll());
         UserView view = new UserView(controller);
         view.run();
     }
